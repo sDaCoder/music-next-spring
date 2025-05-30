10cc2a12032a63e4a9e8ca02a46cb480a49f5bea
sDaCoder <supravadutta_sDa@outlook.com>
2025-05-31 00:33:45 +0530

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page: React.FC = () => {
+  return (
+    <>
+        This is the about page of the app
+        <Button>Click me</Button>
+    </>
+  )
+}
+
+export default page
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold">This is a music app</h1>
+    </>
+  );
+}
+
+export default Home
